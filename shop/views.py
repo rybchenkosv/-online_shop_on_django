36901a441f50754e2aaf169eb13315f7b85d7db8
Sergey R <yourspace.krsk@yandex.ru>
2023-04-10 20:12:45 +0700

    Update

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def shop_list(request):
-    return render(request, 'shop/shop_list.html', {})
+    # СОЗДАЕМ ПЕРЕМЕННУЮ ДЛЯ ОТОБРАЖЕНИЯ ВСЕХ ДАННЫХ, КОТОРЫЕ МЫ ЗАПИХАЛИ В POST И СОРТИРУЕМ ИХ ПО ДАТУ ПУБЛИКАЦИИ
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # РЕАЛИЗУЕМ ПЕРЕМЕННУЮ posts ПОД НАЗВАНИЕМ 'posts' ЧТОБЫ ПЕРЕДАТЬ ЕЕ В ШАБЛОН,
+    # В ПОСЛЕДУЮЩЕМ ВСЕ ПЕРЕМЕННЫЕ МЫ СМОЖЕМ ТАК ЖЕ РЕАЛИЗОВЫВАТЬ И ПЕРЕДАВАТЬ В ШАБЛОН {{ posts }}
+    return render(request, 'shop/shop_list.html', {'posts': posts})
